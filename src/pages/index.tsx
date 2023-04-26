2a34e76a5a3db0bfcdadd5be5e7484b85afb49d5
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-04-26 12:00:48 -0400

    Change text font size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ const PostsView = (props: PostWithUser) => {
           <span>{`@${author.username}`}</span>{" "}
           <span className="font-thin">. {dayjs(post.createdAt).fromNow()}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
