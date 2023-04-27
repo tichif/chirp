638ae12d2b2d5a169beb2a9a50592e97cdcc1924
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-04-27 18:44:30 -0400

    Add routing to Post and Profile page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { SignInButton, useUser } from "@clerk/nextjs";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
+import Link from "next/link";
 
 import { type RouterOutputs, api } from "~/utils/api";
 import CreatePostWizard from "~/components/CreatePostWizard";
@@ -16,22 +17,33 @@ const PostsView = (props: PostWithUser) => {
   const { post, author } = props;
 
   return (
-    <div className="flex gap-3 border-b border-slate-400 p-4">
-      <Image
-        src={author.profilePicture}
-        alt={author.username}
-        className="rounded-full"
-        width={56}
-        height={56}
-      />
-      <div className="flex flex-col ">
-        <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span>{" "}
-          <span className="font-thin">. {dayjs(post.createdAt).fromNow()}</span>
+    <Link href={`/posts/${post.id}`}>
+      <div className="flex gap-3 border-b border-slate-400 p-4">
+        <Link href={`/@${author.username}`}>
+          <Image
+            src={author.profilePicture}
+            alt={author.username}
+            className="rounded-full"
+            width={56}
+            height={56}
+          />
+        </Link>
+        <div className="flex flex-col ">
+          <div className="flex gap-1 text-slate-300">
+            <Link href={`/@${author.username}`}>
+              <span>{`@${author.username}`}</span>{" "}
+            </Link>
+            <Link href={`/posts/${post.id}`}>
+              {" "}
+              <span className="font-thin">
+                . {dayjs(post.createdAt).fromNow()}
+              </span>
+            </Link>
+          </div>
+          <span className="text-2xl">{post.content}</span>
         </div>
-        <span className="text-2xl">{post.content}</span>
       </div>
-    </div>
+    </Link>
   );
 };
 
