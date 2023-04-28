4df12000c9354c526fcff7d32851cb21372e121a
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-04-27 21:08:45 -0400

    Create the Header for the profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -3,9 +3,11 @@ import Head from "next/head";
 import { createServerSideHelpers } from "@trpc/react-query/server";
 import superjson from "superjson";
 import { prisma } from "~/server/db";
+import Image from "next/image";
 
 import { api } from "~/utils/api";
 import { appRouter } from "~/server/api/root";
+import Layout from "~/components/Layout";
 
 const ProfilePage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { username } = props;
@@ -23,7 +25,20 @@ const ProfilePage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
       <Head>
         <title>{data.username}</title>
       </Head>
-      <main className="flex h-screen justify-center">Profile View</main>
+      <Layout>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profilePicture}
+            alt={`Profile of ${data.username as string}`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">@{data.username}</div>
+        <div className="w-full border-b border-slate-400"></div>
+      </Layout>
     </>
   );
 };
@@ -38,8 +53,6 @@ export const getStaticProps = async (
   });
 
   const username = context.params?.slug as string;
-
-  console.log(username.slice(1));
 
   await helpers.profile.getUserByUsername.prefetch({
     username: username.slice(1),
